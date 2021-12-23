02a6081ba32eed25db47b76e86c24db7adf1d263
Den4icGuti <goos7412@gmail.com>
2021-12-23 20:18:25 +0200

    add to fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// const iframeRef = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframeRef);
+const iframeRef = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeRef);
 
-// player.on('play', throttle(function () {
-//   player.getCurrentTime().then(function (sec) {
-//     console.log('title:', sec);
-//     localStorage.setItem('current-time', JSON.stringify(sec))
-//    });
-// },1000),
-// );
-
-// const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
-// //  console.log(toStart);
-// player.setCurrentTime(toStart);
-
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-player.on(
-  'timeupdate',
-  throttle(() => {
-    player.getCurrentTime().then(sec => {
-      localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
-    });
-  }, 1000),
+player.on('play', throttle(function () {
+  player.getCurrentTime().then(function (sec) {
+    console.log('title:', sec);
+    localStorage.setItem('current-time', JSON.stringify(sec))
+   });
+},1000),
 );
-const timeToStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
-player.setCurrentTime(timeToStart);
+
+const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+console.log(toStart);
+player.setCurrentTime(toStart);
+
