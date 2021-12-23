b5ed2bd41b5f7f90bccaf9ecfe4ec1b5fefeb1dc
Den4icGuti <goos7412@gmail.com>
2021-12-23 03:06:36 +0200

    ready first home work

---
+++ src/js/01-gallery.js
@@ -0,0 +1,22 @@
+import simpleLightbox from 'simplelightbox';
+import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css'
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+const marCupGallery = galleryMarcup();
+
+galleryRef.insertAdjacentHTML('beforeend', marCupGallery);
+
+function galleryMarcup() { 
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+ 
+    `
+   }).join('')
+};
+
+let gallery = new simpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
