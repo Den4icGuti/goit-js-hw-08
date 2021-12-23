58a3dd5b5facf74f1f721e63de8a2849dc3fdc29
Den4icGuti <goos7412@gmail.com>
2021-12-23 20:03:31 +0200

    ready second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,30 @@
- import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframeRef = document.querySelector('iframe');
+// const iframeRef = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframeRef);
 
-const player = new Vimeo.Player(iframeRef);
+// player.on('play', throttle(function () {
+//   player.getCurrentTime().then(function (sec) {
+//     console.log('title:', sec);
+//     localStorage.setItem('current-time', JSON.stringify(sec))
+//    });
+// },1000),
+// );
 
-iframeRef.addEventListener('play',onVideoStart)
+// const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+// //  console.log(toStart);
+// player.setCurrentTime(toStart);
 
-player.on('play', function () {
-  console.log('Video play!')
-});
-
-player.getVideoTitle().then(function(time) {
-  console.log('title:', time);
-  localStorage.setItem('current-time',JSON.stringify(time))
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(sec => {
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
     });
-
-const toStart = JSON.parse(localStorage.getItem('current-time'));
-console.log(toStart);
-player.getVideoTitle(toStart);
-
-function onVideoStart(toStart) { 
-   console.log(toStart);
-}
+  }, 1000),
+);
+const timeToStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+player.setCurrentTime(timeToStart);
