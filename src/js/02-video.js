5ab38c277eb6f0d10ab6615a1bf51e7259dfd95d
Den4icGuti <goos7412@gmail.com>
2021-12-23 23:42:20 +0200

    the third task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
 const iframeRef = document.querySelector('iframe');
 const player = new Vimeo.Player(iframeRef);
 
-player.on('play', throttle(function () {
+player.on('timeupdate', throttle(function () {
   player.getCurrentTime().then(function (sec) {
     console.log('title:', sec);
     localStorage.setItem('current-time', JSON.stringify(sec))
@@ -12,7 +12,7 @@ player.on('play', throttle(function () {
 },1000),
 );
 
-const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+const toStart = JSON.parse(localStorage.getItem('current-time'));
 console.log(toStart);
 player.setCurrentTime(toStart);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
+const refs = {
+  form: document.querySelector('.feedback-form'),
+}
 
+const FEEDBAKC_FORM_STATE = "feedback-form-state";
+
+refs.form.addEventListener('submit', throttle(onSubmitForm,500));
+
+populateData()
+function onSubmitForm(e) { 
+  e.preventDefault();
+  
+  const formEl = e.currentTarget.elements;
+  
+  const email = formEl.email.value;
+  const message = formEl.message.value;
+ 
+  const data = {
+    email,
+    message,
+  }
+   localStorage.setItem(FEEDBAKC_FORM_STATE,JSON.stringify(data))
+   console.log('my-data', data);
+  
+  refs.form.reset();
+  localStorage.removeItem(FEEDBAKC_FORM_STATE)
+}
+
+function populateData() { 
+  const saveData = localStorage.getItem(FEEDBAKC_FORM_STATE); { 
+    if (saveData) { 
+      console.log(saveData);
+    }
+  }
+}
