a97a1e4420c11a023b958da52268e0f364b4bb2d
Den4icGuti <goos7412@gmail.com>
2021-12-23 18:08:16 +0200

    get second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+ import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframeRef);
+
+iframeRef.addEventListener('play',onVideoStart)
+
+player.on('play', function () {
+  console.log('Video play!')
+});
+
+player.getVideoTitle().then(function(time) {
+  console.log('title:', time);
+  localStorage.setItem('current-time',JSON.stringify(time))
+    });
+
+const toStart = JSON.parse(localStorage.getItem('current-time'));
+console.log(toStart);
+player.getVideoTitle(toStart);
+
+function onVideoStart(toStart) { 
+   console.log(toStart);
+}
